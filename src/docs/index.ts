d14bc5c1e2288495f7ca62e27cb80a01ea4f57ef
EmJee <mart-janroeleveld@outlook.com>
2021-06-07 20:54:25 +0200

    📚 Added paths to documentation

--- src/docs/index.ts
+++ src/docs/index.ts
@@ -3,9 +3,13 @@
 import basicInfo from './basicInfo.js'
 import servers from './servers.js'
 import tags from './tags.js'
+import components from './components'
+import paths from './paths'
 
 export default {
 	...basicInfo,
 	...servers,
 	...tags,
+	...components,
+	...paths,
 }
