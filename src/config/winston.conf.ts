fc2244fafb34d404e74105dd5a94fad14ebae126
EmJee <mart-janroeleveld@outlook.com>
2021-06-07 19:28:24 +0200

    ⌚ Added timestamps to logger

--- src/config/winston.conf.ts
+++ src/config/winston.conf.ts
@@ -4,10 +4,16 @@ const { APP_NAME } = process.env
 
 const logger = winston.createLogger({
 	level: 'info',
-	format: winston.format.json(),
+	format: winston.format.combine(
+		winston.format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
+		winston.format.json()
+	),
 	defaultMeta: { service: APP_NAME },
 	transports: [
-		new winston.transports.File({ filename: 'logs/error.log', level: 'error' }),
+		new winston.transports.File({
+			filename: 'logs/error.log',
+			level: 'error',
+		}),
 		new winston.transports.File({ filename: 'logs/combined.log' }),
 	],
 })
