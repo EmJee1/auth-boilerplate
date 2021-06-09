e811e7359f84847a877bdfadd40e2b09296d0ddb
EmJee <mart-janroeleveld@outlook.com>
2021-06-09 11:59:22 +0200

    🚏 Better path to ejs template

--- src/vendor/MailTemplate.ts
+++ src/vendor/MailTemplate.ts
@@ -51,7 +51,7 @@ class Mailer {
 
 		const template = templateName || 'email.template.ejs'
 
-		ejs.renderFile(`dist/messages/templates/${template}`, {
+		ejs.renderFile(`../messages/templates/${template}`, {
 			content: this._mailContent,
 		})
 			.then(html => {
