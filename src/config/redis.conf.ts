2bc06eca12376daef14ac5f2835f44ef7b933b80
EmJee <mart-janroeleveld@outlook.com>
2021-06-30 17:08:54 +0200

    🔑 Added redis config to env file

--- src/config/redis.conf.ts
+++ src/config/redis.conf.ts
@@ -1,5 +1,11 @@
 import Redis from 'redis'
 
-const redis = Redis.createClient()
+const { REDIS_HOST, REDIS_PORT, REDIS_PASSWORD } = process.env
+
+const redis = Redis.createClient({
+	host: REDIS_HOST,
+	port: parseInt(REDIS_PORT),
+	password: REDIS_PASSWORD || undefined,
+})
 
 export default redis
